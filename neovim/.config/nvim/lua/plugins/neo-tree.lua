e25213dc1eb0df4cdc514633e77276847689fa1c
You-saku <yousaku.dev19@gmail.com>
2023-06-23 23:31:28 +0900

    delete: 無駄な空白を削除

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -12,9 +12,9 @@ return {
       winbar = true,
       content_layout = "center",
       sources = {
-        { source = "filesystem",  display_name = get_icon("FolderClosed", 1, true) .. "File" },
-        { source = "buffers",     display_name = get_icon("DefaultFile", 1, true) .. "Bufs" },
-        { source = "git_status",  display_name = get_icon("Git", 1, true) .. "Git" },
+        { source = "filesystem", display_name = get_icon("FolderClosed", 1, true) .. "File" },
+        { source = "buffers", display_name = get_icon("DefaultFile", 1, true) .. "Bufs" },
+        { source = "git_status", display_name = get_icon("Git", 1, true) .. "Git" },
         { source = "diagnostics", display_name = get_icon("Diagnostic", 1, true) .. "Diagnostic" },
       },
     },
